ceb20ff379a50bc57e32d8c74f9cd7229b83f4fd
Guillermo Lizasoain <guillermo.lizasoain@bluebamboo.com>
2018-12-21 23:06:17 -0500

    Commit to master 1

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     description: `And the four beasts had each of them six wings about him; and they were full of eyes within: and they rest not day and night, saying, Holy, holy, holy, Lord God Almighty, which was, and is, and is to come. (Rev 4:8)`,
     siteUrl: `http://holyholyholy.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `master`,
     },
   },
   plugins: [
